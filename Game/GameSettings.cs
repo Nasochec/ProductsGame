c3079e36ac6fecd23950d80e5493fbbc5828f1b5
Artemy Gladkov <gladkov.art76@gmail.com>
2023-03-09 17:21:25 +0300

    Initial 2.0

---
+++ Game/GameSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class GameSettings
+    {
+        List<string> productions = new List<string>();
+
+
+
+
+    }
+}
---
+++ ProdictsGame/GameSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class GameSettings
+    {
+        List<string> productions = new List<string>();
+        bool IsBankShare = true;
+        int numberOfPlayers = 2;
+        
+
+
+
+    }
+}
